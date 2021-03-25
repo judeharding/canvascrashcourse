e9e591d2c73f989943e32119493883e850043eba
Jude Harding <jharding@genesisdla.com>
2021-03-25 17:20:57 -0400

    previous work

---
+++ clickdotsscript.js
@@ -0,0 +1,49 @@
+const canvas = document.getElementById("canvas1");
+const ctx = canvas.getContext("2d"); // or webgl instead of 2d
+
+// helps establish the canvas height and width to give you access
+// to match browser window but this only happens on initial load
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+// helps with mobile and resizing window
+window.addEventListener("resize", function () {
+	canvas.width = window.innerWidth;
+	canvas.height = window.innerHeight;
+	// ctx.fillStyle = "White";
+	// ctx.fillRect(10, 10, 150, 50);
+});
+
+// logging the object will allow you to see all of the settings you can modify
+// console.log(ctx);
+// ctx.fillStyle = "white";
+
+// in the dunderproto, you can see drawing methods like arc
+// ctx.arc();
+
+// because this is outside of the resize window listener, this
+// disappears when the window is resized.
+// ctx.fillStyle 10, 10, 150, 50); // x, y, width, height
+
+const mouse = {
+	x: null,
+	y: null,
+};
+
+canvas.addEventListener("click", function (event) {
+	mouse.x = event.x;
+	mouse.y = event.y;
+	drawCircle();
+	console.log(event); // to see your x and y coordinates of a click
+});
+
+function drawCircle() {
+	ctx.fillStyle = "red";
+	// ctx.strokeStyle = "yellow";
+	// ctx.lineWidth = 15;
+	ctx.beginPath();
+	// ctx.arc(100, 100, 50, 0, Math.PI * 2); // fixed location
+	ctx.arc(mouse.x, mouse.y, 50, 0, Math.PI * 2);
+	// ctx.stroke();
+	ctx.fill();
+}
---
+++ script.js
@@ -0,0 +1,53 @@
+function updateForm(width, height, box) {
+	"use strict";
+	var x = 50;
+	var y = 50;
+	var canvas = document.createElement("canvas"); //Create a canvas
+	//Set canvas width/height
+	canvas.style.width = "99%";
+	canvas.style.height = "99%";
+	// Set canvas drawing area width/height
+	canvas.width = window.innerWidth;
+	canvas.height = window.innerHeight;
+	//Position canvas
+	canvas.style.position = "absolute";
+	canvas.style.left = 0;
+	canvas.style.top = 350;
+	canvas.style.pointerEvents = "none";
+	document.body.appendChild(canvas); //Append canvas to body element
+	var context = canvas.getContext("2d");
+	//Draw rectangle
+	context.rect(x, y, height * 50, width * 50);
+	context.fillStyle = "blue";
+	context.fill();
+	switch (box) {
+		case "three-two":
+			canvas.style.zIndex = 3000;
+			context.rect(x, y, 150, 200);
+			context.fillStyle = "blue";
+			context.fill();
+			break;
+		case "four-six":
+			canvas.style.zIndex = 2000;
+			context.rect(x, y, 200, 300);
+			context.fillStyle = "red";
+			context.fill();
+			break;
+		case "five-four":
+			canvas.style.zIndex = 1000;
+			context.rect(x, y, 350, 350);
+			context.fillStyle = "green";
+			context.fill();
+			break;
+		default:
+			alert("The value supplied is out of range!");
+			break;
+	}
+}
+
+function draw() {
+	var width = document.getElementById("twid").value;
+	var height = document.getElementById("lhgt").value;
+	var box = document.getElementById("boxSize").value;
+	updateForm(width, height, box);
+}
