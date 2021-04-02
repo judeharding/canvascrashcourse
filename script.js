5f31122df5def96a9930ef83d855f537124c5685
Jude Harding <jharding@genesisdla.com>
2021-04-02 17:36:54 -0400

    canvas grid - circle - ellipse- clear

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// https://www.youtube.com/c/SteveGriffith-Prof3ssorSt3v3/search?query=canvas
+// https://www.youtube.com/watch?v=uh1Sgj2Gt70&list=PLyuRouwmQCjlqieGcZgsDlGHtHFar-ZOp
 console.log("HELLO");
 let canvas, ctx;
 
@@ -21,6 +21,22 @@ document.addEventListener("DOMContentLoaded", () => {
 
 	var x = 0; // setting the 0x
 	var y = 0; // setting the 0y
+
+	// 	TRY TOMORROW https://www.geeksforgeeks.org/how-to-get-the-coordinates-of-a-mouse-click-on-a-canvas-element/
+	// 	function getMousePosition(canvas, event) {
+	// 		let rect = canvas.getBoundingClientRect();
+	// 		let x = event.clientX - rect.left;
+	// 		let y = event.clientY - rect.top;
+	// 		console.log("Coordinate x: " + x,
+	// 								"Coordinate y: " + y);
+	// }
+
+	// let canvasElem = document.querySelector("canvas");
+
+	// canvasElem.addEventListener("mousedown", function(e)
+	// {
+	// 		getMousePosition(canvasElem, e);
+	// });
 
 	// ========================
 	// DRAW ELLIPSE
