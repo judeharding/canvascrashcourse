4489dd01d2d2cc6ec4c34cc344444e6d37774ee8
Jude Harding <jharding@genesisdla.com>
2021-04-06 14:52:33 -0400

    finished images on canvas

--- imgscript.js
+++ imgscript.js
@@ -7,9 +7,10 @@ document.addEventListener("DOMContentLoaded", (ev) => {
 	canvas.width = 600;
 	canvas.height = 400;
 
-	let imgObj = new Image();
+	let imgObj = new Image(); // fetch img to put it on the canvas
 
 	imgObj.onload = function () {
+		// listener
 		let w = canvas.width;
 		let nw = imgObj.naturalWidth; //incoming height
 		let nh = imgObj.naturalHeight; //incoming width
@@ -17,26 +18,32 @@ document.addEventListener("DOMContentLoaded", (ev) => {
 		let h = w / aspect;
 		console.log("natural width ", nw);
 		console.log("natural height ", nh);
-		console.log("aspect ", aspect);
+		console.log("aspect ", aspect); // if changing canvas width to 600, what is the aspect ratio of the height?
+		console.log("new width ", w);
+		console.log("new height with aspect ratio is now ", h);
 
 		canvas.height = h;
-		ctx.drawImage(imgObj, 0, 0, w, h);
+
+		// ctx.drawImage(imgObj, 0, 0); // will take only the top, left corner of the pic without the aspect ratio applied
+		ctx.drawImage(imgObj, 0, 0, w, h); // passing in new imgObj starting at 0/0 with the new aspect width and height
 		// ctx.drawImage(imgObj, dx, dy);
 		// ctx.drawImage(imgObj, dx, dy, dw, dh);
-		// ctx.drawImage(imgObj, sx, sy, sw, sh, dx, dy, dw, dh);
+		// ctx.drawImage(imgObj, sx, sy, sw, sh, dx, dy, dw, dh); // source x/y, source w/h to get a PIECE of the image below
+		// ctx.drawImage(imgObj, 0, 650, 200, 200 / aspect, 0, 0, w, h);
 	};
 
 	imgObj.src =
-		"https://i0.wp.com/clayandmilk.com/wp-content/uploads/2019/09/tennis-in-out-line-call.jpg?resize=1024%2C719&ssl=1";
+		"https://i0.wp.com/clayandmilk.com/wp-content/uploads/2019/09/tennis-in-out-line-call.jpg";
 
-	//canvas.addEventListener('click', greyscale);
-	canvas.addEventListener("click", colorChannel);
+	canvas.addEventListener("click", greyscale);
+	// canvas.addEventListener("click", colorChannel);
 });
 
 const greyscale = function (ev) {
 	imgData = ctx.getImageData(0, 0, canvas.width, canvas.height);
 	let arr = imgData.data;
 	for (let i = 0; i < arr.length; i = i + 4) {
+		// adding 4 b/c r, g, b and alpha
 		let ttl = arr[i] + arr[i + 1] + arr[i + 2];
 		let avg = parseInt(ttl / 3);
 		arr[i] = avg; //red turns grey
@@ -51,14 +58,15 @@ const colorChannel = function (ev) {
 	imgData = ctx.getImageData(0, 0, canvas.width, canvas.height);
 	let arr = imgData.data;
 	for (let i = 0; i < arr.length; i = i + 4) {
-		arr[i] = 0; //R  removes the red channel
+		// arr[i] = 0; //R  removes the red channel
 		//arr[i+1] = 0;   //G  removes the green channel
 		arr[i + 2] = 0; //B  removes the blue channel
 	}
 	imgData.data = arr;
 	ctx.putImageData(imgData, 0, 0);
 
+	// puts the extracted data from the image and put it into the canvas
 	let img = canvas.toDataURL("image/jpeg", 1.0);
-	console.log(img);
+	console.log("IMAGE... ", img); // provides the base64 encoded image data
 	document.querySelector("img").src = img;
 };
